083d0cc7ebfdc96f46d8dbfad539c3e2305b56b9
cvarvaro <christianvarv@gmail.com>
2023-02-01 17:19:49 -0500

    update

---
+++ ch01/exercises/colors.py
@@ -0,0 +1,12 @@
+colors = ["red", "blue", "green", "yellow", "orange", "purple", "brown", "black", "white",]
+
+print("I have", len(colors), "colors to choose from.")
+print(colors[0])
+print(colors[1])
+print(colors[2])
+print(colors[3])
+print(colors[4])
+print(colors[5])
+print(colors[6])
+print(colors[7])
+print(colors[8])
