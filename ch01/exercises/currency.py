6b96ff8e9b33248cdd4831ec8177291449577329
cvarvaro <christianvarv@gmail.com>
2023-02-06 17:02:40 -0500

    update

--- ch01/exercises/currency.py
+++ ch01/exercises/currency.py
@@ -1,5 +1,8 @@
-import math
-
-rate = float(input("What is the current exchange rate for Dollars and Euros? "))
+rate = float(input("What is the current exchange rate for Euros to Dollars? "))
 amount = float(input("How much money do you want to exchange? "))
 
+total = amount * rate
+
+result = total - 3.00
+printed__result = round(result, 2)
+print(printed__result)
--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,22 @@
+import random
+#Part A
+weeks = 16
+classes = 5
+tuition = 6000
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
 
+classes_per_week = 3
+cost_per_class = cost_per_week/classes_per_week
+print("Cost per class is equal to", cost_per_class)
+
+print(weeks, type(weeks))
+print(classes, type(classes))
+print(tuition, type(tuition))
+print(cost_per_week, type(cost_per_week))
+print(classes_per_week, type(classes_per_week))
+print(cost_per_class, type(cost_per_class))
+
+# Part B
+high_school_classes = ["Math", "Science", "English", "History", "Gym"]
+print("My favorite class in high school was", random.choice(high_school_classes))
