ccae999b03e203a87e02de56d915ba8634b7e2be
Christian Varvaro <cvarvar1@binghamton.edu>
2023-02-06 13:35:11 -0500

    ch01

---
+++ ch01/exercises/currency.py
@@ -0,0 +1,5 @@
+import math
+
+rate = float(input("What is the current exchange rate for Dollars and Euros? "))
+amount = float(input("How much money do you want to exchange? "))
+
---
+++ ch01/exercises/data.py
@@ -0,0 +1,14 @@
+print(10*5)
+print(10**2)
+print(15/10) # Standard division, always results in a float
+print(15//10) # Always results in an integer, floor operation
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10/15)
+
+# I think the last answer is wrong because it is a constantly repeating number that
+# can never really be exact because it is a float and can only store so many numbers. 
+
